bc112ad19c9ac2ed5e985bf008c463e61ea10386
Sreerama <stripuramallu3@gatech.edu>
2018-04-11 03:05:19 -0400

    line graph integration

--- line.js
+++ line.js
@@ -1,38 +1,134 @@
-var lineChart = function(workload, timestampList) {
-    var container = '<div id="workloadDropdown"></div><div id="linesgraphs"><div class="row"><div class="column"><div id="graph1"><div id="apache"></div></div><div id="tomcat"></div></div></div><div class="row"><div class="column"><div id="cdjb"></div><div id="mysql"></div></div></div></div>'
-    document.getElementById("linegraphs").innerHTML(container); 
+function lines(workload, timestampList) {
+    table = '<table style="width:100%"><tr><td><div id="graph1"></div></td><td><div id="graph2"></div></td></tr><br><tr><td><div id="graph3"></div></td><td><div id="graph4"></div></td></tr></table>'
+    document.getElementById("linegraphs").innerHTML = table; 
     console.log(workload)
+    workload = 1000; 
     timestamp_min = timestampList[0]
-    timestam_max = timestampList[timestampList.length - 1]
+    timestamp_max = timestampList[timestampList.length - 1]
+    for (var i = 1; i <= 4; i++) {
+        plotGraph("#graph" + i, workload, timestamp_min, timestamp_max, "response_time_1000.csv")
+    }
+}
 
-    var margin = {top: 20, right: 20, bottom: 110, left: 40},
-    margin2 = {top: 430, right: 20, bottom: 30, left: 40},
-    width = 950, 
-    height = 500
+function plotGraph(div_id, workload, timestamp_min, timestamp_max, csv_file) {
+    console.log(div_id)
+    function filter(d) {
+      d.timestamp = +d.date_time;
+      d.value = +d.total_http;
+      /*if (d.timestamp >= timestamp_min && d.timestamp <= timestamp_max && d.workload == workload) {
+        return d; 
+      }*/
+      return d;
+    }
+    var margin = {top: 10, right: 10, bottom: 100, left: 40},
+        margin2 = {top: 430, right: 10, bottom: 20, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom,
+        height2 = 500 - margin2.top - margin2.bottom;
 
-    d3.select("graph1").append("svg")
+    var x = d3.time.scale().range([0, width]),
+        x2 = d3.time.scale().range([0, width]),
+        y = d3.scale.linear().range([height, 0]),
+        y2 = d3.scale.linear().range([height2, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom"),
+        xAxis2 = d3.svg.axis().scale(x2).orient("bottom"),
+        yAxis = d3.svg.axis().scale(y).orient("left");
+
+    var brush = d3.svg.brush()
+        .x(x2)
+        .on("brush", brushed);
+
+    var area = d3.svg.area()
+        .interpolate("cardinal")
+        .x(function(d) { return x(d.timestamp); })
+        .y0(height)
+        .y1(function(d) { return y(d.value); });
+
+    var area2 = d3.svg.area()
+        .interpolate("cardinal")
+        .x(function(d) { return x2(d.timestamp); })
+        .y0(height2)
+        .y1(function(d) { return y2(d.value); });
+
+    var svg = d3.select(div_id).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+    svg.append("defs").append("clipPath")
+        .attr("id", "clip")
+      .append("rect")
+        .attr("width", width)
+        .attr("height", height);
+
+    var focus = svg.append("g")
+        .attr("class", "focus")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var context = svg.append("g")
+        .attr("class", "context")
+        .attr("transform", "translate(" + margin2.left + "," + margin2.top + ")");
+
+    var zoom = d3.behavior.zoom()
+        .on("zoom", draw);
+
+    var rect = svg.append("svg:rect")
+        .attr("class", "pane")
         .attr("width", width)
         .attr("height", height)
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+        .call(zoom);
 
-    height2 = height
+    d3.csv(csv_file, filter, function(error, data) {
+      x.domain(d3.extent(data.map(function(d) { return d.timestamp; })));
+      y.domain([0, d3.max(data.map(function(d) { return d.value; }))]);
+      x2.domain(x.domain());
+      y2.domain(y.domain());
 
+      zoom.x(x);
+      focus.append("path")
+          .datum(data)
+          .attr("class", "area")
+          .attr("d", area);
 
-    var x = d3.scaleTime().range([0, width - margin.left - margin.bottom]),
-        x2 = d3.scaleTime().range([0, width - margin.left- margin.bottom]),
-        y = d3.scaleLinear().range([height - margin.top - margin.right, 0]),
-        y2 = d3.scaleLinear().range([height2 - margin.top - margin.right, 0]);
+      focus.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
 
-    var xAxis = d3.axisBottom(x),
-        xAxis2 = d3.axisBottom(x2),
-        yAxis = d3.axisLeft(y);
+      focus.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
 
-    var brush = d3.brushX()
-        .extent([[0, 0], [width, height2]])
-        .on("brush end", brushed);
+      context.append("path")
+          .datum(data)
+          .attr("class", "area")
+          .attr("d", area2);
 
-    var zoom = d3.zoom()
-        .scaleExtent([1, Infinity])
-        .translateExtent([[0, 0], [width, height]])
-        .extent([[0, 0], [width, height]])
-        .on("zoom", zoomed);   
+      context.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height2 + ")")
+          .call(xAxis2);
+
+      context.append("g")
+          .attr("class", "x brush")
+          .call(brush)
+        .selectAll("rect")
+          .attr("y", -6)
+          .attr("height", height2 + 7);
+    });
+    function brushed() {
+      x.domain(brush.empty() ? x2.domain() : brush.extent());
+      focus.select(".area").attr("d", area);
+      focus.select(".x.axis").call(xAxis);
+      zoom.x(x);
+    }
+
+    function draw() {
+      focus.select(".area").attr("d", area);
+      focus.select(".x.axis").call(xAxis);
+      // Force changing brush range
+      brush.extent(x.domain());
+      svg.select(".brush").call(brush);
+    }
 }
