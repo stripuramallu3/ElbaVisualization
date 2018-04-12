ed030faed76147945c6976821e01dc25bacd5161
sbonala3 <sbonala3@gatech.edu>
2018-04-12 03:22:14 -0400

    Fixed script to merge queue length data :)

--- cleandata.py
+++ cleandata.py
@@ -2,54 +2,25 @@ import os
 import datetime
 import re
 
-# RWfname = 'pointtime_RW.tsv' 
-# ROfname = 'pointtime_RO.tsv' 
-# header = "workload\ttimestamp\tpit\n"
-# RWout=open(RWfname,"w")
-# RWout.write(header)
-# ROout=open(ROfname,"w")
-# ROout.write(header)
-
-# os.chdir("output")
-# for x in os.walk("."):
-# 	if "RW" in x[0]:
-# 		workload = re.findall('\d+', x[0])[0]
-# 		os.chdir(x[0])
-# 		pitRW = open("Pointintime.csv")
-# 		next(pitRW)
-# 		count = 0
-# 		for line in pitRW:
-# 			splitLine  = line.split(",");
-# 			RWout.write(workload + "\t" + str(count) +  "\t" + splitLine[1])
-# 			count = count + 50
-# 		pitRW.close()
-# 		os.chdir("../")
-# 	elif "RO" in x[0]:
-# 		workload = re.findall('\d+', x[0])[0]
-# 		os.chdir(x[0])
-# 		pitRO = open("Pointintime.csv")
-# 		next(pitRO)
-# 		count = 0
-# 		for line in pitRO:
-# 			splitLine  = line.split(",");
-# 			ROout.write(workload + "\t" + str(count) +  "\t" + splitLine[1])
-# 			count = count + 50
-# 		pitRO.close()
-# 		os.chdir("../")
-# RWout.close()
-# ROout.close()
 def main():
 	mergePitData("RW")
 	mergePitData("RO")
-	mergeQLengthData("reponsetime")
-	# mergeQLengthData("multiplicity")
-	# mergeQLengthData("inout")
+	mergeQLengthData("responsetime", "RO")
+	mergeQLengthData("responsetime", "RW")
+	mergeQLengthData("multiplicity", "RO")
+	mergeQLengthData("multiplicity", "RW")
+	mergeQLengthData("inout", "RO")
+	mergeQLengthData("inout", "RW")
+
 
 def mergePitData(config):
+	#print(next(os.walk("."))[1])
 	fname = 'pointtime_'+config+'.tsv'
 	header = "workload\ttimestamp\tpit\n"
-	fout = RWout=open(RWfname,"w")
+	fout = open(fname,"w")
 	fout.write(header)
+	origDir = os.getcwd()
+	print(os.getcwd())
 	os.chdir("output")
 	for x in os.walk("."):
 		if config in x[0]:
@@ -60,16 +31,39 @@ def mergePitData(config):
 			count = 0
 			for line in pitFile:
 				splitLine  = line.split(",");
-				RWout.write(workload + "\t" + str(count) +  "\t" + splitLine[1])
+				fout.write(workload + "\t" + str(count) +  "\t" + splitLine[1] + "\n")
 				count = count + 50
 			pitFile.close()
-			os.chdir("../")
+	os.chdir(origDir)
+	print(os.getcwd())
 	fout.close()
 
-def mergeQLengthData(metric):
-	fout = metric + '.tsv'
-	header = "workload\ttype\tdate_time\ttotal_http\n"
-	fout = RWout=open(RWfname,"w")
+def mergeQLengthData(metric, config):
+	fname = metric + config + '.csv'
+	header = "workload,type,date_time,total_http\n"
+	fout = open(fname,"w")
 	fout.write(header)
+	pathToHome = os.getcwd()
 	os.chdir("output")
-	for x in os.walk("."):
+	for root, dirs, files in os.walk("."):
+		for dir in dirs:
+			pathToOutput = os.getcwd()
+			os.chdir(dir)
+			for sroot, sdirs, sfiles in os.walk("."):
+				for name in sfiles:
+					if metric in name:
+	   					if config in name:
+		   					splitName = name.split('_')
+		   					serverType = splitName[1]
+		   					workload = re.findall('\d+', splitName[3])[0]
+		   					datafile = open(name)
+		   					next(datafile)
+		   					count = 0;
+		   					for line in datafile:
+		   						splitLine = line.split(",")
+		   						fout.write(workload + "," + serverType + "," + str(count) +"," + splitLine[1] + ",")
+		   						count = count + 50
+			os.chdir(pathToOutput)
+	os.chdir(pathToHome)
+
+main()
